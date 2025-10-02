e9cfc6b9f5742486534a6151ff82e7c07a687268
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2025-10-02 11:12:19 -0300

    Updated files for development

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -8,5 +8,7 @@ uv run python manage.py wait_for_database
 uv run python manage.py migrate
 # import icons
 uv run python manage.py import_pictograms --attribution "Maki Icons by Mapbox" /app/custom/icons
+# create tile layers
+sh scripts/import-tilelayers.sh
 # run app
 uv run gunicorn -b 0.0.0.0:8000 wsgi
