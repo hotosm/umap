f4d796a82a3c5a51bbd1ee6fe899225f5416bc5a
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2025-10-02 14:50:10 -0300

    Updated files for development

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 set -eo pipefail
-# import tilelayers
-echo "Importing tilelayers..."
-psql \
-  --host="$UMAP_DB_HOST" \
-  --port="${UMAP_DB_PORT:-5432}" \
-  --username="$UMAP_DB_USER" \
-  --dbname="$UMAP_DB_NAME" \
-  --file=/app/scripts/import-tilelayers.sql
 # collect static files
 uv run python manage.py collectstatic --noinput
 # now wait for the database
@@ -16,5 +8,13 @@ uv run python manage.py wait_for_database
 uv run python manage.py migrate
 # import icons
 uv run python manage.py import_pictograms --attribution "Maki Icons by Mapbox" /app/custom/icons
+# import tilelayers
+echo "Importing tilelayers..."
+psql \
+  --host="$UMAP_DB_HOST" \
+  --port="${UMAP_DB_PORT:-5432}" \
+  --username="$UMAP_DB_USER" \
+  --dbname="$UMAP_DB_NAME" \
+  --file=/app/scripts/import-tilelayers.sql
 # run app
 uv run gunicorn -b 0.0.0.0:8000 wsgi
